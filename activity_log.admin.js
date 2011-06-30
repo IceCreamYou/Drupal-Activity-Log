488da60e2a81508842d7ec1317db4be2487531bd
Isaac Sukin <ice@icecreamyou.com>
2011-06-30 13:06:42 -0400

    Fixed some of the things around grouping messages not getting stored correctly

--- activity_log.admin.js
+++ activity_log.admin.js
@@ -2,20 +2,20 @@ Drupal.behaviors.activityLogAdmin = function (context) {
   // Make sure we can run context.find().
   var ctxt = $(context);
   var handle = function() {
-    var val = ctxt.find('input:radio[name=group_method]:checked').val();
+    var val = ctxt.find('input:radio[name="settings[grouping][group_method]"]:checked').val();
     if (val == 'target_action') {
-      ctxt.find('#edit-group-interval-wrapper').hide();
-      ctxt.find('#edit-group-max-wrapper').hide();
-      ctxt.find('#edit-short-template-wrapper').hide();
+      ctxt.find('#edit-settings-grouping-group-interval-wrapper').hide();
+      ctxt.find('#edit-settings-grouping-group-max-wrapper').hide();
+      ctxt.find('#edit-settings-grouping-group-summary-wrapper').hide();
       ctxt.find('#activity-log-admin-description').hide();
     }
     else if (val == 'action' || val == 'user_action') {
-      ctxt.find('#edit-group-interval-wrapper').show();
-      ctxt.find('#edit-group-max-wrapper').show();
-      ctxt.find('#edit-short-template-wrapper').show();
+      ctxt.find('#edit-settings-grouping-group-interval-wrapper').show();
+      ctxt.find('#edit-settings-grouping-group-max-wrapper').show();
+      ctxt.find('#edit-settings-grouping-group-summary-wrapper').show();
       ctxt.find('#activity-log-admin-description').show();
     }
   };
   handle();
-  ctxt.find('input:radio[name=group_method]').change(handle);
+  ctxt.find('input:radio[name="settings[grouping][group_method]"]').change(handle);
 }
