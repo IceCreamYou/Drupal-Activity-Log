e487d4d03b749ae62e507eff49381b8473a92114
Isaac Sukin <ice@icecreamyou.com>
2011-07-06 19:42:57 -0400

    Change the way we designate that similar messages should be grouped from leaving the group template blank to an option in the group method setting

--- activity_log.admin.js
+++ activity_log.admin.js
@@ -7,15 +7,24 @@ Drupal.behaviors.activityLogAdmin = function (context) {
       ctxt.find('#edit-settings-grouping-group-interval-wrapper').hide();
       ctxt.find('#edit-settings-grouping-group-max-wrapper').hide();
       ctxt.find('#edit-settings-grouping-group-summary-wrapper').hide();
-      ctxt.find('#edit-settings-grouping-collection-method-wrapper').hide();
+      ctxt.find('#edit-settings-grouping-collapse-method-wrapper').hide();
+      ctxt.find('#edit-settings-grouping-group-template-wrapper').show();
       ctxt.find('#activity-log-admin-description').hide();
     }
     else if (val == 'action' || val == 'user_action') {
       ctxt.find('#edit-settings-grouping-group-interval-wrapper').show();
       ctxt.find('#edit-settings-grouping-group-max-wrapper').show();
       ctxt.find('#edit-settings-grouping-group-summary-wrapper').show();
-      ctxt.find('#edit-settings-grouping-collection-method-wrapper').show();
+      ctxt.find('#edit-settings-grouping-collapse-method-wrapper').show();
+      ctxt.find('#edit-settings-grouping-group-template-wrapper').show();
       ctxt.find('#activity-log-admin-description').show();
+    }
+    else if (val == 'none') {
+      ctxt.find('#edit-settings-grouping-group-interval-wrapper').hide();
+      ctxt.find('#edit-settings-grouping-group-max-wrapper').hide();
+      ctxt.find('#edit-settings-grouping-group-summary-wrapper').hide();
+      ctxt.find('#edit-settings-grouping-collapse-method-wrapper').hide();
+      ctxt.find('#edit-settings-grouping-group-template-wrapper').hide();
     }
   };
   handle();
