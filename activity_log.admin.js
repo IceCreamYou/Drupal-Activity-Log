867dcc2b8ac9d9b2c139f1fe951ed9500bab97c4
Isaac Sukin <ice@icecreamyou.com>
2011-07-07 23:39:28 -0400

    Tasks #2.02 and 2.03: add stream owner checkboxes to Rules action form and conditionally show exposed fields

--- activity_log.admin.js
+++ activity_log.admin.js
@@ -1,6 +1,8 @@
 Drupal.behaviors.activityLogAdmin = function (context) {
   // Make sure we can run context.find().
   var ctxt = $(context);
+
+  // Show/hide grouping settings.
   var handle = function() {
     var val = ctxt.find('input:radio[name="settings[grouping][group_method]"]:checked').val();
     if (val == 'target_action') {
@@ -29,4 +31,30 @@ Drupal.behaviors.activityLogAdmin = function (context) {
   };
   handle();
   ctxt.find('input:radio[name="settings[grouping][group_method]"]').change(handle);
+
+  // Show/hide stream owner exposed fields.
+  ctxt.find('#edit-settings-stream-owner-id-wrapper').hide();
+  ctxt.find('#edit-settings-stream-owner-type-wrapper').hide();
+  var fields = Drupal.settings.activity_log.stream_owner_expose_fields;
+  $.each(fields, function(key, value) {
+    ctxt.find('input:checkbox[name="settings[stream_owner_entity_group]['+ key +']"]').change(function() {
+      var shown = new Array();
+      $.each(fields, function(k, v) {
+        for (var val in v) {
+          if ($('input:checkbox[name="settings[stream_owner_entity_group]['+ k +']"]').attr('checked')) {
+            if ($.inArray(v[val], shown) == -1) {
+              $('#edit-settings-stream-owner-'+ v[val] +'-wrapper').show();
+              shown[v[val]] = v[val];
+            }
+          }
+        }
+      });
+      var f = ['id', 'type'];
+      for (var val in f) {
+        if (shown[f[val]] == undefined || shown[f[val]] == null) {
+          $('#edit-settings-stream-owner-'+ f[val] +'-wrapper').hide();
+        }
+      }
+    });
+  });
 }
